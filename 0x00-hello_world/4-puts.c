2b847dbf565479a4da60cfa871776d3f67b8ba0c
Fikayomi31 <fikayoogundijo@gmail.com>
2022-09-08 07:48:27 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *  
- * Description: prints Programming is like building a multilingual puzzle
- *    
- * Return: Always 0 (Success)
- *       
+ * main - Entry point  
+ * Description: prints Programming is like building a multilingual puzzle    
+ * Return: Always 0 (Success)       
 */
 int main(void)
 {
