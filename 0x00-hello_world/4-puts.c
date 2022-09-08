33ba932695b2bb8b8e12a19c237dbd2a946bb7ef
Fikayomi31 <fikayoogundijo@gmail.com>
2022-09-08 07:04:04 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * wrinting a program that
+ * print Programming
+ * is like building a multilingual
+ *  puzzle
+*/
+int main(void)
+{
+	put("Programming is like building a multilingual puzzle"\n);
+	return (0);
+}
