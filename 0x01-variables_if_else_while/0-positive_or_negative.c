e50ac2b7047bc1ebee203dbab08800c8adb39c18
Fikayomi31 <fikayoogundijo@gmail.com>
2022-09-09 10:28:34 -0700

    positive better than negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -21,11 +21,11 @@ int main(void)
 
 	if (n > 0)
 	{	
-		printf("%d is positive\n", n)
+		printf("%d is positive\n", n);
 	}
 	else if (n ==  0)
 	{
-		printf("%d is zero\n", n)
+		printf("%d is zero\n", n);
 	}
 	else
 	{
