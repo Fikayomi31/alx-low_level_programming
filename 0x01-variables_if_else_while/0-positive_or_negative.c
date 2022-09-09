8cd49641fb57c1826c03db7675f3039d318a6c00
Fikayomi31 <fikayoogundijo@gmail.com>
2022-09-09 10:22:42 -0700

    positive better than negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,12 +18,20 @@ int main(void)
 
 	srand(time(0));
 	n = rand() -RAND_MAX / 2;
+
 	if (n == 0)
+	{	
 		printf("%d is zero\n", n)
+	}
 	else if (n < 0)
+	{
 		printf("%d is negative\n", n)
+	}
 	else
+	{
 		printf("%d is positive\n", n);
+	}
 	
 	return (0);
+
 }
