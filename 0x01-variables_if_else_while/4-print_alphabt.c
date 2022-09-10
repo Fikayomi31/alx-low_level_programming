a855b965e1f755aa945670374585616675c38d31
Fikayomi31 <fikayoogundijo@gmail.com>
2022-09-09 18:42:21 -0700

    alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,13 +12,10 @@ int main(void)
 	while (ch <= 'z')
 	{
 		if (ch != 'e' && ch != 'q')
-												{	
-										
-													putchar(ch);
-	
-												}		
+		{
+			putchar(ch);
+		}
 		ch++;
-	
 	}
 	putchar('\n');
 	return (0);
