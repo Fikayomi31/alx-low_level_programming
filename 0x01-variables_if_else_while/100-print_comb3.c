d53cc85ceab90c72c0bd6d88932fa2c35545233b
Fikayomi31 <fikayoogundijo@gmail.com>
2022-09-10 07:33:59 -0700

    comvination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,17 +13,12 @@ int main(void)
 	/* as long as we are not at the last digit */
 	while (start < end)
 	{
-		/**
- 		 * current should be next digit after the starting point 		 	  * this is because we do not want to combine the same number
-		 * e.g when you're starting from five, current should be 6.
-		 * i.e 56
-		 */
 		current = start + 1;
 		while (current <= end)
 		{
 			putchar(start);
 			putchar(current);
-													 if (!(start == end - 1 && current == end))
+			if (!(start == end - 1 && current == end))
 			{
 				putchar(44);
 				putchar(32);
