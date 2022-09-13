4f3e78312a704530c7ffc49e68b7c410e0143b2f
Fikayomi31 <fikayoogundijo@gmail.com>
2022-09-13 09:54:28 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+/* program to print _putchar */
+#include <main.h>
+/**
+ * main - Entry point of _putchar
+ * Description: print _putchar follow by new line
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+0. Write a program that prints _putchar, followed by a new line.
+1. 
