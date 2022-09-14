1043541f387eaaadf5126bc09da3593ba50fb4bf
Fikayomi31 <fikayoogundijo@gmail.com>
2022-09-13 19:06:35 -0700

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * main - print the firat 50 Fibonacci
+ * Description: starting with 1 and 2 followed by new line
+ * Return 0 (success)
+ */
+int main(void)
+{
+	long int i, j, k, next;
+
+	j = 1;
+	c = 2;
+	for (a = 1; a <= 50 ++a)
+	{
+		if (b != 20365011074)
+		{
+			printf("%ld, ", b);
+		}
+		else
+		{
+			print("%ld\n", b);
+		}
+		next = b + c;
+		b = c;
+		c = next;
+	}
+	return (0);
+}
