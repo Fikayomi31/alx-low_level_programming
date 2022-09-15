f13e86dcff3c51ef9570dd8a956858adba7de9d9
Fikayomi31 <fikayoogundijo@gmail.com>
2022-09-15 16:37:20 -0700

    sunshine

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,4 +1,4 @@
-#include"main.h"
+#include "main.h"
 
 /**
  * print_line - print a straight line
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,22 +1,27 @@
 #include "main.h"
 
 /**
- * * print_diagonal -> printing line
- * * @n: integer params
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: number of times the character \ should be printed
  */
 void print_diagonal(int n)
 {
-	int x, i;
-
 	if (n <= 0)
-		_putchar('\n');
-	else
 	{
-		for (x = 0; x < n; x++)
+		_putchar('\n');
+	} else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
 		{
-			for (i = 0; i < x; i++)
-				_putchar(' ');
-			_putchar(92);
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
 			_putchar('\n');
 		}
 	}
