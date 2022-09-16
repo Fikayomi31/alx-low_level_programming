c991bc8d8bdae5b71cfee8c7f40d998e334d4421
Fikayomi31 <fikayoogundijo@gmail.com>
2022-09-15 19:41:59 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,18 +10,11 @@
 
 void print_line(int n)
 {
-	if (n <= 0)
-
+	
+	for (; n > 0; n--)
 	{
-		_putchar('\n');
-	} else
-	{
-		int i;
-
-		for (i = 1; i <= n; i++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
+		_putchar('_');
 	}
+	_putchar('\n');
+
 }
