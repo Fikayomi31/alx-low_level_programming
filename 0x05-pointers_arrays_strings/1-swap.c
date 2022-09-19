ea9751a7c05e6985e6890b01f4e89c5047a4b4e6
Fikayomi31 <fikayoogundijo@gmail.com>
2022-09-19 14:46:47 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - swapping the value of two integer
+ * @a: first integer
+ * @b: second integer
+ * Return: failure
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+	*a = *b
+	*b = c
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 0x05. C - Pointers, arrays and strings
 0. Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.
-1. 
+1. Write a function that swaps the values of two integers.
+2.  
