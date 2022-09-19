9f303097538f9f0333c749a2632dc06ee4950109
Fikayomi31 <fikayoogundijo@gmail.com>
2022-09-19 15:27:10 -0700

    strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * int_strlen - running a program to return the length of the string
+ * @s: a string
+ * Reture: the length of the string
+ */
+int_strlen(char *s)
+{
+	int length + 0;
+
+	while (*s != '\0')
+	{
+		length++
+		s = s + 1;
+	}
+	return (length);
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,4 +1,4 @@
 0x05. C - Pointers, arrays and strings
 0. Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.
 1. Write a function that swaps the values of two integers.
-2.  
+2. Write a function that returns the length of a string.  
