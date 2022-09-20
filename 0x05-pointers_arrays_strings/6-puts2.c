c390d7c6f37e576b46ae5d6556e948c5e2cde3d5
Fikayomi31 <fikayoogundijo@gmail.com>
2022-09-19 20:57:22 -0700

    6-puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,12 +7,12 @@
 void puts2(char *str)
 {
 	int i = 0;
-	
-	while (str[i] != '\0'; i++)
+
+	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
 		{
-			_putchar(s[i]);
+			_putchar(str[i]);
 		}
 		i++;
 	}
