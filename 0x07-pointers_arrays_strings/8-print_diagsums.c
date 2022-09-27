b2c83b84558c876e27e57ffd45b7c7947c73fdda
Fikayomi31 <fikayoogundijo@gmail.com>
2022-09-26 17:51:00 -0700

    array

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - sum of teo diadonals of
+ * square matrix of integer
+ * @a: sum of the integer
+ * @size: size of matrix
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i];
+		a += size;
+	}
+
+	a -= size;
+
+	for (i = 0; i < size; i++)
+	{
+		sum2 += a[i];
+		a -= size;
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
