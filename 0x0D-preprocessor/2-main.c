b4bcf82f7a29d3e91e6bbf4fe9fe27a17058bdff
Fikayomi31 <fikayoogundijo@gmail.com>
2022-10-10 09:43:41 -0700

    macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file it was compiled
+ * 	from and followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Name of source file: %s\n", __FILE__);
+	return (0);
+}
