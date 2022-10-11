0cf04a99ee3251a66cfc0eaa0c5e2f7be88ed1db
Fikayomi31 <fikayoogundijo@gmail.com>
2022-10-10 19:58:35 -0700

    struct

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_dog - free dogs
+ * @d: dog to be free
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
