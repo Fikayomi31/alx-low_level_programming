ad7199a5579569deaca12dd59419c95d2a55933f
Fikayomi31 <fikayoogundijo@gmail.com>
2022-10-12 13:42:55 -0700

    function pointer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,7 +13,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
 	for (i = 0; i < size; i++)
-		if (cmo(array[i]) != 0)
+		if (cmp(array[i]) != 0)
 			return (i);
 	return (-1);
 }
