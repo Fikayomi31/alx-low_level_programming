6081c4c00c481157af219e9228997a94015a5b7c
Fikayomi31 <fikayoogundijo@gmail.com>
2022-10-16 09:47:29 -0700

    correction

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,7 +12,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < n; i++)
 		sum += va_arg(args, int);
 	va_end(args);
 	return (sum);
