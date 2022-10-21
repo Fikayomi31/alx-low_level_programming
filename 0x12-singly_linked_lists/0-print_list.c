c3e8a394542c2c3ca09603f6e3a86621b48c31f9
Fikayomi31 <fikayoogundijo@gmail.com>
2022-10-20 20:41:00 -0700

    linker

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,11 +7,20 @@
  */
 size_t print_list(const list_t *h)
 {
-	if (h == NULL)
-		return (0);
-	if (h->str == NULL)
-		printf("[0] (nil)\n");
-	else
-		printf("[%u] %s\n", h->len, h->str);
-	return (1 + print-list(h->next);
+	size_t nodes = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+
 }
