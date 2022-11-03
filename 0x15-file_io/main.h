25bbc646b8dd2d48fb3ecf8528d303d7bb8a173c
Fikayomi31 <fikayoogundijo@gmail.com>
2022-11-03 04:01:41 -0700

    file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,6 +8,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct file - a file structure
+ * @name: name of the file
+ * @fd: file descriptor
+ */
+struct file
+{
+	char *name;
+	int fd;
+};
+
+/* alias for struct file */
+typedef struct file file_t;
+
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
