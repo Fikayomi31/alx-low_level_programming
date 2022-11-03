a40f597559d665d72124a208e63da69a9140ce83
Fikayomi31 <fikayoogundijo@gmail.com>
2022-11-03 04:11:36 -0700

    files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,7 +19,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == 1)
 		return (0);
 	/* allocating memory for the letters */
-	buffer = malloc(sizeof(char) *letters + 1);
+	buffer = malloc(sizeof(char) * letters + 1);
 	if (buffer == NULL)
 		return (0);
 
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!len)
 		return (1);
-	
+
 	if (write(fd, text_content, len) == -1)
 		return (-1);
 	close(fd);
