135499588ed8a26f7ee28d3a329cf6d286ae83a2
Fikayomi31 <fikayoogundijo@gmail.com>
2023-01-06 12:34:11 -0800

    betty ooo

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
 
 /**
- * hash_tables_creat - Create a hash table.
+ * hash_table_create - Create a hash table.
  * @size: The size of the array.
  * Return: If an error occurs - NULL.
- * 	Otherwise - a pointer to the new hash table.
+ * Otherwise - a pointer to the new hash table.
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
