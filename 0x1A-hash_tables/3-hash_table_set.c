a4bcf7432cbcfd3457cde4a72aff43ef5ace636b
Fikayomi31 <fikayoogundijo@gmail.com>
2023-01-06 14:25:50 -0800

    add

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,12 +14,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new;
 	char *value_copy;
 	unsigned long int index, i;
-	
+
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-	
 	value_copy = strdup(value);
-	if (value_copy ==NULL)
+	if (value_copy == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
