15bbe63022fea2cca43efab0dcffe3e61301d7b5
Fikayomi31 <fikayoogundijo@gmail.com>
2023-02-26 15:30:50 -0800

    num

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,15 +7,27 @@
  */
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int num;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -num;
+		num = -nn
 	}
-	if ((num / 10) > 0)
+	else
+	{
+		num = n;
+	}
+
+	if (n < num)
+	{
+		_putchar('0' + num)
+	}
+	else
+	{
+		((num / 10) > 0)
 		print_number(num / 10);
 
-	_putchar((num % 10) + '0');
+		_putchar((num % 10) + '0');
+	}
 }
