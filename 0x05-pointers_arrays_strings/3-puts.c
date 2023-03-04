260279332cbe6c1853da7def6db78633cf01d48a
Fikayomi31 <fikayoogundijo@gmail.com>
2023-03-04 15:06:14 -0800

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _putsb- function that prints a string
+ * _puts - function that prints a string
  * @str: the string
  *
  * Return: void
@@ -10,7 +10,7 @@
 void _puts(char *str)
 {
 	int i;
-	
+
 	for (i = 0; str[i] != '\0'; i++)
 		_putchar(str[i]);
 	_putchar('\n');
