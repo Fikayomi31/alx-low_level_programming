a14a67498718099e9458b90bfc2fcf8b0781641b
Fikayomi31 <fikayoogundijo@gmail.com>
2023-03-05 16:58:35 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other character of a string starting with the
+ * first character followed by new line
+ * @str: the string
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		if ((str[i] % 2) == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
