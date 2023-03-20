2f415945391ffe7552948753a39566e83d5b0282
Fikayomi31 <fikayoogundijo@gmail.com>
2023-03-20 16:56:12 -0700

    header

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -24,9 +24,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int add(int a, int b);
-int sub(int a, int b);
-int mul(int a, int b);
-int div(int a, int b);
+
 
 #endif /* MAIN_H */
