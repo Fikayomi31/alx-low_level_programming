61fbd2b1864e075af4e4842e61a825ed1f30448e
Fikayomi31 <fikayoogundijo@gmail.com>
2023-10-11 13:54:33 +0000

    time and space complexity

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,43 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - search array using binary search algorithms
+ * @array: the array
+ * @size: size of element in the array
+ * @value: value to search for
+ * 
+ * Return: index where value is located or -1
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	int right, left, mid;
+	int i;
+
+	if (array == NULL)
+		return (-1);
+	left = 0;
+	right = size -1;
+	
+	while (left <= right)
+	{
+		mid = (left + right) / 2;
+
+		printf("Searching in array: ");
+
+		for (i = left; i <= right; i++)
+			printf("%i"
+
+	if (array[mid] == target)
+		return (mid);
+
+	if (array[mid] < value)
+	{
+		left = mid + 1;
+	}
+	else
+	{
+		right = mid - 1;
+	}
+
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,5 +3,7 @@
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
 
 #endif /* SEARCH_ALGOS_H */
