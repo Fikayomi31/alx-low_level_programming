8a037880451a65e28aae8931c24fbe91801ce533
Fikayomi31 <fikayoogundijo@gmail.com>
2023-10-11 18:06:17 +0000

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -5,7 +5,7 @@
  * @array: the array
  * @size: size of element in the array
  * @value: value to search for
- * 
+ *
  * Return: index where value is located or -1
  */
 
@@ -17,8 +17,8 @@ int binary_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 	left = 0;
-	right = size -1;
-	
+	right = size - 1;
+
 	while (left <= right)
 	{
 		mid = (left + right) / 2;
@@ -26,18 +26,18 @@ int binary_search(int *array, size_t size, int value)
 		printf("Searching in array: ");
 
 		for (i = left; i <= right; i++)
-			printf("%i"
+			/**
+			 * print new line if i == last digit
+			 * or , if it is not equal to last digit
+			 */
+			printf("%d%s", array[i], i == right ? "\n" : ", ");
+		if (array[mid] == value)
+			return (mid);
 
-	if (array[mid] == target)
-		return (mid);
-
-	if (array[mid] < value)
-	{
-		left = mid + 1;
+		if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
 	}
-	else
-	{
-		right = mid - 1;
-	}
-
+	return (-1);
 }
